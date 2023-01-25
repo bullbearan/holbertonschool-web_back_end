f975d6bffe802f130ce0ec967e937a66ebeb690f
bullbearan <4403@holbertonstudents.com>
2023-01-25 12:37:39 +0100

    add

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,7 +6,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     "this is a line of text"
-    lst = []
-    async for i in async_generator():
-        lst.append(i)
+    lst = [i async for i in async_generator()]
     return lst
