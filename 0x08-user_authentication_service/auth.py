b0a6b01c78518f3c66ea0b3bf0a27226f1796152
bullbearan <4403@holbertonstudents.com>
2023-02-17 23:38:04 +0100

    add

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -4,6 +4,8 @@ from db import DB
 import bcrypt
 import uuid
 from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 
 def _hash_password(password: str) -> str:
@@ -25,12 +27,13 @@ class Auth:
 
     def register_user(self, email: str, password: str) -> User:
         "This is a line of text"
-        user = self._db.find_user_by(email=email)
-        if user:
-            raise ValueError(f"User {email} already exists")
-        hashed_password = _hash_password(password)
-        new_user = self._db.add_user(email, hashed_password)
-        return new_user
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
+        raise ValueError(f"User {email} already exists")
 
     def valid_login(self, email: str, password: str) -> bool:
         "This is a line of text"
