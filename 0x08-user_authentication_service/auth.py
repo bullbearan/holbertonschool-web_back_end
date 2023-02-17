1ebe4d5d43eefc51ec6d44d006dc1fbe00a0ad67
bullbearan <4403@holbertonstudents.com>
2023-02-17 23:55:16 +0100

    add

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -54,7 +54,7 @@ class Auth:
     def get_user_from_session_id(self, session_id: str) -> str:
         "This is a line of text"
         try:
-            return user = self._db.find_user_by(session_id=session_id)
+            return self._db.find_user_by(session_id=session_id)
         except Exception:
             return None
 
