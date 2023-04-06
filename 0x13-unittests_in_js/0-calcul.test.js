8e0b46b55f751a75cbf5ddf35fb79e24659c40ca
bullbearan <4403@holbertonstudents.com>
2023-04-06 15:24:36 +0100

    add

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -2,20 +2,16 @@ const assert = require("assert");
 const calculateNumber = require("./0-calcul.js");
 
 describe("calculateNumber", () => {
-	it("test1", () => {
+	it("test", () => {
 		assert.strictEqual(calculateNumber(2, 2), 4);
 		assert.strictEqual(calculateNumber(1, 5.5), 7);
 		assert.strictEqual(calculateNumber(1.5, 2.5), 5);
 		assert.strictEqual(calculateNumber(1.5, 2), 4);
 		assert.strictEqual(calculateNumber(-2, -2), -4);
 		assert.strictEqual(calculateNumber(-1, -5.5), -6);
-	});
-	it("test2", () => {
-		assert.strictEqual(calculateNumber(2, 2), 4);
-		assert.strictEqual(calculateNumber(1, 5.5), 7);
-		assert.strictEqual(calculateNumber(1.5, 2.5), 5);
-		assert.strictEqual(calculateNumber(1.5, 2), 4);
-		assert.strictEqual(calculateNumber(-2, -2), -4);
-		assert.strictEqual(calculateNumber(-1, -5.5), -6);
+		assert.strictEqual(isNaN(calculateNumber("one", 1)), true);
+		assert.strictEqual(isNaN(calculateNumber(2, "two")), true);
+		assert.strictEqual(isNaN(calculateNumber(5.5)), true);
+		assert.strictEqual(isNaN(calculateNumber()), true);
 	});
 });
