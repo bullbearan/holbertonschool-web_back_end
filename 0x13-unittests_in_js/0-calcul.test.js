cda202728fef75caa3a560dde473774a6a807499
bullbearan <4403@holbertonstudents.com>
2023-04-06 15:19:23 +0100

    check

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -2,7 +2,15 @@ const assert = require("assert");
 const calculateNumber = require("./0-calcul.js");
 
 describe("calculateNumber", () => {
-	it("test", () => {
+	it("test1", () => {
+		assert.strictEqual(calculateNumber(2, 2), 4);
+		assert.strictEqual(calculateNumber(1, 5.5), 7);
+		assert.strictEqual(calculateNumber(1.5, 2.5), 5);
+		assert.strictEqual(calculateNumber(1.5, 2), 4);
+		assert.strictEqual(calculateNumber(-2, -2), -4);
+		assert.strictEqual(calculateNumber(-1, -5.5), -6);
+	});
+	it("test2", () => {
 		assert.strictEqual(calculateNumber(2, 2), 4);
 		assert.strictEqual(calculateNumber(1, 5.5), 7);
 		assert.strictEqual(calculateNumber(1.5, 2.5), 5);
